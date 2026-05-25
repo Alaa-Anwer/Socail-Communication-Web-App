6c6c0ed4edd0edf6a454323aaefd150e5fe354c7
Alaa Anwar <anwaralaa286@gmail.com>
2026-05-25 10:51:29 +0300

    feat(client): setup Clerk provider and router in main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from "@clerk/clerk-react";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+import { Provider } from 'react-redux';
+import { store } from "./app/store.js";
+createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </ClerkProvider>,
+);
