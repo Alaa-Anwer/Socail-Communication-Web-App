34200a1f640f7ece4b23471cd7e688a266a0e291
Alaa Anwar <anwaralaa286@gmail.com>
2026-05-30 09:11:37 +0300

    feat(client): add StoryViewer full-screen component

---
+++ client/src/components/StoryViewer.jsx
@@ -0,0 +1,107 @@
+import { BadgeCheck, X } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const StoryViewer = ({ viewedStory, setViewedStory }) => {
+    const handleClose = () => {
+        setViewedStory(null);
+    };
+    const renderContent = () => {
+        switch (viewedStory.media_type) {
+            case "image":
+                return (
+                    <img
+                        src={viewedStory.media_url}
+                        className="max-w-full max-h-screen object-contain"
+                    />
+                );
+
+            case "video":
+                return (
+                    <video
+                        src={viewedStory.media_url}
+                        onEnded={() => setViewedStory(null)}
+                        controls
+                        autoPlay
+                        className="max-h-screen"
+                    />
+                );
+
+            case "text":
+                return (
+                    <div className="w-full h-full flex items-center justify-center p-8 text-white text-2xl text-center">
+                        {viewedStory.content}
+                    </div>
+                );
+
+            default:
+                return null;
+        }
+    };
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        let timer, progressInterval;
+
+        if (viewedStory && viewedStory.media_type !== "video") {
+            setProgress(0);
+
+            const duration = 10000;
+            const setTime = 100;
+            let elapsed = 0;
+
+            progressInterval = setInterval(() => {
+                elapsed += setTime;
+                setProgress((elapsed / duration) * 100);
+            }, setTime);
+
+            timer = setTimeout(() => {
+                setViewedStory(null);
+            }, duration);
+        }
+
+        return () => {
+            clearTimeout(timer);
+            clearInterval(progressInterval);
+        };
+    }, [viewedStory, setViewedStory]);
+
+  return (
+      <div
+          className="fixed inset-0 h-screen bg-black bg-opacity-90 z-110 flex items-center justify-center"
+          style={{
+              backgroundColor:
+                  viewedStory.media_type === "text"
+                      ? viewedStory.background_color
+                      : "#000000",
+          }}
+      >
+          <div className="absolute top-0 left-0 w-full h-1 bg-gray-700">
+              <div
+                  className="h-full bg-white transition-all duration-100 linear"
+                  style={{ width: `${progress}%` }}
+              ></div>
+          </div>
+
+          <div className="absolute top-4 left-4 flex items-center space-x-3 p-2 px-4 sm:p-4 sm:px-8 backdrop-blur-2xl rounded bg-black/50">
+              <img
+                  src={viewedStory.user?.profile_picture}
+                  alt=""
+                  className="size-7 sm:size-8 rounded-full object-cover border border-white"
+              />
+
+              <div className="text-white font-medium flex items-center gap-1.5">
+                  <span>{viewedStory.user?.full_name}</span>
+                  <BadgeCheck size={18} />
+              </div>
+          </div>
+          <button onClick={handleClose} className="absolute top-4 text-white text-3xl font-bold focus:outline-none ">
+              <X className="w-8 h-8 hover:scale-110 transition cursor-pointer" />
+          </button>
+          <div className= "max-w-[90vw] flex items-center justify-center">
+              {renderContent()}
+          </div>
+      </div>
+  )
+}
+
+export default StoryViewer
